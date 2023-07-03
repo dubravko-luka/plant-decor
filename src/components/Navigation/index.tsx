98929ae4e727ed94a00df935bf1660df90348fe9
Phuong Nguyen <dev.richard.npm98@gmail.com>
2023-07-03 21:04:06 +0700

    update code

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -32,25 +32,23 @@ const Navigation: React.FC = () => {
       <header className={`${styles.navigation} ${isHidden ? styles.hidden : ''} ${scrolled ? styles.active : ''} relative justify-between flex z-9`}>
         <ul className="flex items-center gap-20">
           <li className={styles.itemMenu}>
-            <a className="text-white font-bold text-base" href="/">Home</a>
+            <a className="text-white font-bold text-base" href="/">Trang chu</a>
           </li>
           <li className={styles.itemMenu}>
-            <a className="text-white font-bold text-base" href="/">Products</a>
+            <a className="text-white font-bold text-base" href="/">Tu van & Thiet ke</a>
           </li>
         </ul>
         <ul className="flex items-center gap-20">
           <li className={styles.itemMenu}>
-            <a className="text-white font-bold text-base" href="tel:+84380897654">+84 380 897 654</a>
+            <a className="text-white font-bold text-base" href="/">San pham</a>
           </li>
           <li className={styles.itemMenu}>
-            <a className="text-white font-bold text-base" href="https://zalo.me/3380897654" target="_blank">
-              Zalo
-            </a>
+            <a className="text-white font-bold text-base" href="/" target="_blank">Lien he</a>
           </li>
         </ul>
         <div className={`${styles.logo} text-center`}>
-          <p className="name text-3xl text-white font-bold tracking-wider">LONG DESIGN</p>
-          <p className="description text-base text-white font-medium uppercase tracking-wider">Aesthetics & beauty</p>
+          <p className="name text-3xl text-white font-bold tracking-wider">S-DECOR</p>
+          <p className="description text-base text-white font-medium uppercase tracking-wider">Architecture & Interior Design</p>
         </div>
       </header>
     </>
